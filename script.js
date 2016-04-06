1664fc7184dbf33ee3b018ddcb441b495f40b971
dnzk <danishkalesaran@gmail.com>
2016-04-06 23:02:53 +0700

    basic pie chart http://zeroviscosity.com/d3-js-step-by-step/step-1-a-basic-pie-chart

--- script.js
+++ script.js
@@ -2,5 +2,39 @@
 
   'use strict';
 
-  console.log(d3);
+  var dataset = [
+    { label: 'Abulia', count: 10 },
+    { label: 'Betelgeuse', count: 20 },
+    { label: 'Cantaloupe', count: 30 },
+    { label: 'Djikstra', count: 40 }
+  ];
+
+  var width = 360;
+  var height = 360;
+  var radius = Math.min(width, height) / 2;
+
+  var color = d3.scale.category20b();
+
+  var svg = d3.select('#chart')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('transform', 'translate(' + (width / 2) + ', ' + (height / 2) + ')');
+
+  var arc = d3.svg.arc()
+    .outerRadius(radius);
+
+  var pie = d3.layout.pie()
+    .value(function(d) { return d.count; })
+    .sort(null);
+
+  var path = svg.selectAll('path')
+    .data(pie(dataset))
+    .enter()
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', function(d, i) {
+      return color(d.data.label);
+    });
 })(d3);
